1b66fa218a18dc09aacf4d39e6e5042bb218758c
bharath926 <31567943+bharath926@users.noreply.github.com>
2017-09-03 15:42:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articleonet={
+var articleone={
     title:'article-one',
     heading:'article-one',
     date:'aug9,2017',
