1a005794f6a4a4ad5ae2840a8322d8d55fc45ff7
bharath926 <31567943+bharath926@users.noreply.github.com>
2017-09-03 14:50:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,15 +9,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function(req, res){
-    res.sendFile('article one is requested');
+    res.send('article one is requested');
 });
-app.get('/article-two', function(req, res){
-    res.sendFile('article two is requested');
-});
-app.get('/article-threee', function(req, res){
-    res.sendFile('article three is requested');
-});
-
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
