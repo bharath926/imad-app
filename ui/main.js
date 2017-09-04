8a6a7ce730caa6243cb973f3b80953d1dab74d70
bharath926 <31567943+bharath926@users.noreply.github.com>
2017-09-04 23:57:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,15 @@
-console.log('Loaded!');
-var element=document.getElementById('main-text');
-element.innerHTML='this is another proces of figure';
-var img=document.getElementById('madi');
-marginRight=0;
-function moveLeft(){
-    marginRight=marginRight+1;
-    img.style.marginRight=marginRight+'px';
-}
-img.onclick=function(){
-    var interval=setInterval(moveLeft,50);
+var button=document.getElementById('çounter');
+button.onclick= function(){
+    var request=new XMLHttpRequest();
+    request.onreadystatechange=function(){
+        if(request.readystate==XMLHttpRequest.DONE){
+            if(request.status==200){
+                var counter=request.response.Text;
+                var span=document.getElementById('count');
+                  span.InnerHtml=counter.toString();
+            }
+        }
+    };
+    request.open('GET','http://http://polisettybharath7.imad.hasura-app.io/counter',true);
+    request.send(null);
 };
