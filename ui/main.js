fda85b712c354da0b84e8b44d50d4ff8a0e12426
bharath926 <31567943+bharath926@users.noreply.github.com>
2017-09-04 18:17:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,10 +3,10 @@ var element=document.getElementById('main-text');
 element.innerHTML='this is another proces of figure';
 var img=document.getElementById('madi');
 marginLeft=0;
-function moveRight(){
-    marginLeft=marginLeft+1;
-    img.style.marginLeft=marginLeft+'px';
+function moveLeft(){
+    marginRight=marginRight+1;
+    img.style.marginRight=marginRight+'px';
 }
 img.onclick=function(){
-    var interval=setInterval(moveRight,50);
+    var interval=setInterval(moveLeft,50);
 };
