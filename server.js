bcef39006dd311a69253c27ce93657770f3d8aa6
bharath926 <31567943+bharath926@users.noreply.github.com>
2017-09-04 17:56:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ app.get('/:articlename', function(req, res){
   res.send(createtemplate(articles[articlename]));
 });
 
-app.get('/ui/style.css', function (req, res) {
+app.get('/ui/style.css', function  (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 app.get('/ui/main.js', function (req, res) {
