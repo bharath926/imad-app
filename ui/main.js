e7897095080127d9050129c0d0bd2bd3bef26408
bharath926 <31567943+bharath926@users.noreply.github.com>
2017-09-04 18:02:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,7 @@
 console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.InnerHTML='this is another proces of figure';
+var img=document.getElementById('madi');
+img.onclick=function(){
+    img.style.marginleft='100px';
+};
